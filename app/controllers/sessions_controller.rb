2175e6884d9cf3c204ed7f55f0a8e436afe87a7b
Tony Peloza <adpeloza@gmail.com>
2020-01-01 18:15:03 -0600

    Lots of work under the hood with Devise, getting closer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < Devise::SessionsController  
+    clear_respond_to
     respond_to :json
+
+    def create
+        byebug
+        self.resource = warden.authenticate!(auth_options)
+        set_flash_message!(:notice, :signed_in)
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource
+    end
 end
--- app/javascript/components/views/Logout.jsx
+++ app/javascript/components/views/Logout.jsx
@@ -3,12 +3,31 @@ import React from 'react';
 class Logout extends React.Component{
     constructor(props){
         super(props);
+
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout(){
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        const url = "/users/sign_out"
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                "X-CSRF-Token": token,
+            },
+          }).then(response => {
+            if(response.ok){
+              return response.json();
+            }
+            throw new Error("Issue with network response. Oops!")
+          }).then(response => {
+            console.log(response);
+        }).catch(error => console.log(error.message));
     }
 
     render(){
         return(
-            <>
-            </>
+            <button onClick={this.handleLogout} className="btn btn-primary">Logout</button>
         )
     }
 }
